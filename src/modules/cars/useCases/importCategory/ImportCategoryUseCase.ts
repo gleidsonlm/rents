05ff4f3eb9d6ffcc7c68cc70a9786b3b4545e31d
GleidsonLM <gleidsonlm@gmail.com>
2022-11-11 10:34:17 -0300

    add: import category loads file and create objects in repository

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,18 +1,54 @@
+import { ICategoriesRepository } from '../../repositories/ICategoriesRepository'
 import { parse as csvParse } from "csv-parse";
 import fs from 'fs';
 
+interface IImportCategory {
+    name: string;
+    description: string;
+}
+
 class ImportCategoryUseCase {
+    constructor( private categoriesRepository: ICategoriesRepository ) {}
 
-    execute(file:Express.Multer.File):void {
-        const stream = fs.createReadStream(file.path);
-        
-        const parseFile = csvParse();
+    loadCategories(file:Express.Multer.File):Promise<IImportCategory[]> {
+        return new Promise((resolve, reject) => {
+            const categories:IImportCategory[] = [];
 
-        stream.pipe(parseFile);
+            const stream = fs.createReadStream(file.path);
+            
+            const parseFile = csvParse();
+    
+            stream.pipe(parseFile);
+    
+            parseFile.on('data', async (row:any) => {
+                const [name, description] = row;
+                categories.push({ name, description });
+            })
+            .on('end', () => {
+                resolve(categories);
+            })
+            .on('error', (err) => {
+                reject(err);
+            })
+        });
+    };
+    
+    async execute(file:Express.Multer.File):Promise<IImportCategory[]> {
+        const categories = await this.loadCategories(file);
+        const createdCategories:IImportCategory[] = [];
 
-        parseFile.on('data', async (row:any) => {
-            console.log(row);
+        categories.map((category) => {
+            const { name, description } = category;
+            const existCategory = this.categoriesRepository.findByName(name);
+
+            if (!existCategory) {
+                this.categoriesRepository.create({ name, description });
+                createdCategories.push({ name, description });
+            }
         })
+        // todo: return the created categories 
+        // console.log( createdCategories )
+        return createdCategories;
     }
 }
 export { ImportCategoryUseCase }
