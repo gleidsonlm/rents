266585cab6a8bdc5828efae31cebb0a676c8aa52
GleidsonLM <gleidsonlm@gmail.com>
2022-12-03 09:06:55 -0300

    fix: register entities in typeorm for inject to work

--- src/modules/cars/useCases/importCategory/ImportCategoryController.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryController.ts
@@ -4,12 +4,13 @@ import { container } from 'tsyringe';
 import { ImportCategoryUseCase } from './ImportCategoryUseCase';
 
 class ImportCategoryController {
-    async handle( request: Request, response: Response ): Promise<Response> {
+    handle( request: Request, response: Response ): Response {
         try {
             const { file } = request;
+
             const importCategoryUseCase = container.resolve(ImportCategoryUseCase);
 
-            await importCategoryUseCase.execute(file)
+            importCategoryUseCase.execute(file)
 
             return response.status(200).send();            
         } catch (error) {
